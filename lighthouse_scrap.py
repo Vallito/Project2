948377d572a598c89cdbd96ec2ef86d0be4155c8
Vallito <vallito34@gmail.com>
2019-01-29 21:26:41 -0500

    tables scrapped

--- lighthouse_scrap.py
+++ lighthouse_scrap.py
@@ -16,6 +16,27 @@ browser.visit(url)
 html = browser.html
 soup = bs(html,'html.parser')
 
+url = []
 #find desired data
 for i in soup.find('ul').findAll('a'):
-    print (i['href'])
+    url.append(i['href'])
+
+
+    #click link to go to country
+   # browser.click_link_by_partial_href(i['href'])
+   # browser.back()
+
+    #obtain new page html and parse
+    #html_country = browser.html
+    #soup_country = bs(html_country,"html.parser")
+
+    #find all tables
+    #wikitables = soup.findAll("table", class_ ='wikitable').findall()
+    #pd.read_html("https://en.wikipedia.org" + i["href"])
+    #if i > 1:
+    #     break
+tables = []
+for i in url:
+    tables.append(pd.read_html("https://en.wikipedia.org" + i))
+
+print(len(tables))
