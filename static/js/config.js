cacdde1303fd46385b6cc67d0e1fc37af17d1c54
Fail Harder <40308537+lborgias@users.noreply.github.com>
2019-02-05 18:49:51 -0500

    merge html

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 // API key
 const API_KEY = "pk.eyJ1Ijoia3VsaW5pIiwiYSI6ImNpeWN6bjJ0NjAwcGYzMnJzOWdoNXNqbnEifQ.jEzGgLAwQnZCv9rA6UTfxQ";
-=======
-const API_KEY = "";
->>>>>>> 20578957bb6b4e038c294ffe650c9f4411bc1549
